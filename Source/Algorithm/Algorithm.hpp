bd612469c840aae2af898d514bc6d685caf9e0af
idanhalp <idanhal27@gmail.com>
2024-10-23 20:55:51 +0300

    Created `Algorithm` namespace.

--- Source/Algorithm/Algorithm.hpp
+++ Source/Algorithm/Algorithm.hpp
@@ -1,4 +1,15 @@
 #ifndef SOURCE_ALGORITHM_HPP
 #define SOURCE_ALGORITHM_HPP
 
+#include "../Parameters.hpp"
+
+namespace Algorithm
+{
+	/// @brief Finds a solution to the input.
+	/// @param input The algorithm's arguments.
+	/// @return A solution if exists, or a relevant `SolutionError` otherwise.
+	/// @note Does not assume for valid input.
+	auto solve(const Params::Input& input) -> Params::output_t;
+};
+
 #endif // SOURCE_ALGORITHM_HPP
