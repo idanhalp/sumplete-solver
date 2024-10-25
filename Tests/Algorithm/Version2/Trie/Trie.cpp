51fa5c0ad2a60b6357c5a9d8b361daaebe87f4be
idanhalp <idanhal27@gmail.com>
2024-10-25 15:00:10 +0300

    Implemented `trie` tests.

--- Tests/Algorithm/Version2/Trie/Trie.cpp
+++ Tests/Algorithm/Version2/Trie/Trie.cpp
@@ -4,5 +4,29 @@
 
 auto Tests::Version2::run_trie_tests() -> void
 {
+	Algorithm::Version2::Utils::Trie trie;
 
+	const std::vector sequence_1{true, true, false, false, true};
+	const std::vector sequence_2{false, false, true, true, true};
+
+	trie.insert(sequence_1);
+	trie.insert(sequence_2);
+
+	const std::vector subsequence_1 = sequence_1;
+	assert(trie.check_if_prefix_exists(subsequence_1));
+
+	const std::vector subsequence_2 = sequence_2;
+	assert(trie.check_if_prefix_exists(subsequence_2));
+
+	const std::vector subsequence_3{true, true};
+	assert(trie.check_if_prefix_exists(subsequence_3));
+	
+	const std::vector subsequence_4{false, false};
+	assert(trie.check_if_prefix_exists(subsequence_4));
+	
+	const std::vector subsequence_5{true, false};
+	assert(!trie.check_if_prefix_exists(subsequence_5));
+	
+	const std::vector subsequence_6{false, true};
+	assert(!trie.check_if_prefix_exists(subsequence_6));
 }
