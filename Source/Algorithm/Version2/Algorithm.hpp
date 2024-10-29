b01385854a707cd831ee1c0a0245a3a83560b7c8
idanhalp <idanhal27@gmail.com>
2024-10-30 00:31:31 +0200

    Added description to `Algorithm::Version2`.

--- Source/Algorithm/Version2/Algorithm.hpp
+++ Source/Algorithm/Version2/Algorithm.hpp
@@ -3,6 +3,19 @@
 
 #include "Source/Parameters.hpp"
 
+/// @brief Optimized algorithm for finding a solution.
+///
+/// The main difference from the first version lays 
+/// in the pruning of dead end paths.
+///
+/// Every possible subset of each row and column is checked,
+/// and the subsets that sum to the target are inserted to a trie.
+/// 
+/// Thus after every substitution we are able to check if we have
+/// reached a dead end, and continue or backtrack accordingly.
+///
+/// This version is much superior to the first one, and is capable
+/// of solving a master-level 9x9 grids immediately.
 namespace Algorithm::Version2
 {
 	/// @brief Finds a solution to the input.
