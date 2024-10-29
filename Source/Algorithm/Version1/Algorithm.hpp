abb655cee9e8265512705db6808c98e39f93decf
idanhalp <idanhal27@gmail.com>
2024-10-30 00:01:26 +0200

    Added description to `Algorithm::Version1`.

--- Source/Algorithm/Version1/Algorithm.hpp
+++ Source/Algorithm/Version1/Algorithm.hpp
@@ -3,6 +3,23 @@
 
 #include "Source/Parameters.hpp"
 
+/// @brief Simple algorithm to find a solution.
+///
+/// Iterates over each cell and tries to keep or
+/// delete it.
+///
+/// When an entire row or column is filled, it checks 
+/// whether the sum is correct, and continues or backtracks
+/// accordingly.
+///
+/// This implementation is sufficient for solving simple grids.
+/// However it struggles with larger grids, taking more than
+/// 20 seconds to solve an easy 9x9 grid, and unable to solve
+/// a master-level grid of such size.
+///
+/// The main drawback of the implementation is the pruning is
+/// very lax: only when an end of a row or column is reached;
+/// an issue which is solved in version 2.  
 namespace Algorithm::Version1
 {
 	/// @brief Finds a solution to the input.
