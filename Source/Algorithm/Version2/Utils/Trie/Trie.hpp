6403bf6122b980632db7c81e793434bf6577f2c0
idanhalp <95182382+idanhalp@users.noreply.github.com>
2024-10-31 15:04:05 +0200

    Changed `Trie`'s documentation.
    Removed the irrelevant example.

--- Source/Algorithm/Version2/Utils/Trie/Trie.hpp
+++ Source/Algorithm/Version2/Utils/Trie/Trie.hpp
@@ -9,12 +9,6 @@
 /// It helps checking if some set is a subset of some other set that was inserted to the trie.
 /// It is useful to efficiently check whether a subset of a row or a column is part of some set that
 /// adds up to the desired sum.
-/// @example Assume some row has 5 elements, and the only way to achieve its target sum is to take its 
-/// first, third and fourth elements.
-/// If we take the first and third elements, we know this is a subset of a possible solution,
-/// so we should continue searching.
-/// However, if we choose the first and second elements, we can see right away that it is not a subset of a solution,
-/// and prune the search.
 namespace Algorithm::Version2::Utils
 {
 	class Trie
