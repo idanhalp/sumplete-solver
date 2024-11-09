8e63f422ab9b1b10ee1d4a14e47d029b7bc3c2d7
idanhalp <idanhal27@gmail.com>
2024-11-09 21:00:09 +0200

    Initial implementation of `MainModule`.

--- Source/Frontend/MainModule/MainModule.hpp
+++ Source/Frontend/MainModule/MainModule.hpp
@@ -1,4 +1,20 @@
 #ifndef SOURCE_FRONTEND_MAIN_MODULE_HPP
 #define SOURCE_FRONTEND_MAIN_MODULE_HPP
 
+#include "Source/Frontend/Components/Grid/Grid.hpp"
+#include <QObject>
+#include <QtQml/qqml.h>
+
+class MainModule : public QObject
+{
+	Q_OBJECT
+	QML_ELEMENT
+
+public:
+	auto resize_grid(size_t new_size) -> void;
+
+private:
+	Components::Grid* m_grid;
+};
+
 #endif // SOURCE_FRONTEND_MAIN_MODULE_HPP
