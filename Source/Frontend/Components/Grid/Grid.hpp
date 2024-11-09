13171c1cad1445643be100d0ed80d12344fefd01
idanhalp <idanhal27@gmail.com>
2024-11-09 21:01:11 +0200

    Initial header of `Components::Grid`.

---
+++ Source/Frontend/Components/Grid/Grid.hpp
@@ -0,0 +1,20 @@
+#ifndef SOURCE_FRONTEND_COMPONENTS_GRID_HPP
+#define SOURCE_FRONTEND_COMPONENTS_GRID_HPP
+
+#include <QObject>
+
+namespace Components
+{
+	class Grid : public QObject
+	{
+		Q_OBJECT
+
+	public:
+
+
+	private:
+		size_t size;
+	};
+}
+
+#endif // SOURCE_FRONTEND_COMPONENTS_GRID_HPP
