4bdd77f00ae562f990a7213f0fc52e9c1f31e183
idanhalp <idanhal27@gmail.com>
2024-11-10 22:47:14 +0200

    Initial implementation of `Components::Grid`.

--- Source/Frontend/Components/Grid/Grid.hpp
+++ Source/Frontend/Components/Grid/Grid.hpp
@@ -1,7 +1,9 @@
 #ifndef SOURCE_FRONTEND_COMPONENTS_GRID_HPP
 #define SOURCE_FRONTEND_COMPONENTS_GRID_HPP
 
+#include <QList>
 #include <QObject>
+#include <optional>
 
 namespace Components
 {
@@ -10,10 +12,14 @@ namespace Components
 		Q_OBJECT
 
 	public:
+		auto operator[](size_t row, size_t col) -> std::optional<int>&;
+		auto operator[](size_t row, size_t col) const -> std::optional<int>;
 
+		auto clear_values() -> void;
+		auto change_size(const size_t new_size) -> void;
 
 	private:
-		size_t size;
+		QList<QList<std::optional<int>>> data;
 	};
 }
 
