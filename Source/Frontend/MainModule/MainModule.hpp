68b73c88a65ca0de4c6faaa3156db0d7032f6f83
idanhalp <idanhal27@gmail.com>
2024-11-13 19:54:26 +0200

    Removed field `m_grid`.
    All modules should be under `MainModule`. No need to break down each
    one.

--- Source/Frontend/MainModule/MainModule.hpp
+++ Source/Frontend/MainModule/MainModule.hpp
@@ -1,7 +1,6 @@
 #ifndef SOURCE_FRONTEND_MAIN_MODULE_HPP
 #define SOURCE_FRONTEND_MAIN_MODULE_HPP
 
-#include "Source/Frontend/Components/Grid/Grid.hpp"
 #include <QObject>
 #include <QtQml/qqml.h>
 
@@ -14,7 +13,7 @@ public:
 	auto resize_grid(size_t new_size) -> void;
 
 private:
-	Components::Grid* m_grid;
+
 };
 
 #endif // SOURCE_FRONTEND_MAIN_MODULE_HPP
