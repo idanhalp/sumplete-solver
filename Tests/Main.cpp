6e9061641c9150d591d7a7795d72b93cb3ee0155
idanhalp <idanhal27@gmail.com>
2024-11-15 21:00:57 +0200

    Updated tests log.
    Added a print statement indicating whether all tests passed.

--- Tests/Main.cpp
+++ Tests/Main.cpp
@@ -14,5 +14,16 @@ auto main(const int num_of_arguments, char* arguments[]) -> int
 		| run(ValidSubsetsGenerationTests())
 		| run(ValidityChecksTests());
 
+	const bool all_tests_passed = status == 0;
+
+	if (all_tests_passed)
+	{
+		qDebug() << "All the tests passed!";
+	}
+	else
+	{
+		qDebug() << "Some of the tests failed!";
+	}
+
 	return status;
 }
