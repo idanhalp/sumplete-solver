56da9402e8355e0367dd4b8d1625c7c8ce0f1e87
idanhalp <idanhal27@gmail.com>
2024-11-16 19:47:17 +0200

    Removed redundant functions.

--- Source/Frontend/MainModule/MainModule.cpp
+++ Source/Frontend/MainModule/MainModule.cpp
@@ -14,24 +14,15 @@ MainModule::MainModule()
 
 auto MainModule::rowCount(const QModelIndex& parent) const -> int
 {
-	Q_UNUSED(parent);
+	Q_UNUSED(parent)
 
 	return m_grid_buffer.count(); // Size.
 }
 
 auto MainModule::data(const QModelIndex& index, int role) const -> QVariant
 {
-
-}
-
-auto MainModule::removeRows(int row, int count, const QModelIndex& parent) -> bool
-{
-
-}
-
-auto MainModule::roleNames() const -> QHash<int, QByteArray>
-{
-	return {};
+	Q_UNUSED(index)
+	Q_UNUSED(role)
 }
 
 auto MainModule::clear() -> void
