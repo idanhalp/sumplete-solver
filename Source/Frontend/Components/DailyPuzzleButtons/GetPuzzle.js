f124ce8464945e0816603436beae18a2e59b55fc
idanhalp <idanhal27@gmail.com>
2024-12-27 20:55:46 +0200

    Removed commented-out code.

--- Source/Frontend/Components/DailyPuzzleButtons/GetPuzzle.js
+++ Source/Frontend/Components/DailyPuzzleButtons/GetPuzzle.js
@@ -40,30 +40,3 @@ function getDailyPuzzle(puzzleSize) {
 
 	xhr.send();
 }
-
-// function generate(ns, day) {
-//       let xhr = new XMLHttpRequest();
-//       // let pf = day - (day % 100) + 1;
-//       let pf = day - (((day - 1) % 100) + 1) + 1;
-//       xhr.open('GET', 'https://data.sumplete.com/daily/' + ns + '/' + pf + '.txt', true);
-//       xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-//           let puzzlelines = xhr.responseText.split(/\n/);
-//           for (let i = 0; i < puzzlelines.length; i++) {
-//             let z = puzzlelines[i].split(/\:/);
-//             if (z[0] == day) {
-//               let y = z[1].split(/;/);
-//               let numberGrid = y[0].split(',');
-//               let rowSums = y[1].split(',');
-//               let colSums = y[2].split(',');
-//               let statusGrid = y[3].split(',');
-//               localStorage.setItem('dailytime', 0);
-//               localStorage.setItem('dailylastplayed', day);
-//               updateboard(day, ns, 1, numberGrid, statusGrid, rowSums, colSums);
-//               break;
-//             }
-//           }
-//         }
-//       };
-//       xhr.send();
-//     }
