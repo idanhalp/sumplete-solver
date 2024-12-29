7d7df9fa64b22793e9282dc28866dc450ab4972e
idanhalp <idanhal27@gmail.com>
2024-12-29 22:22:28 +0200

    Refactored `Trie.cpp`.

--- Source/Backend/Algorithm/Version2/Utils/Trie/Trie.cpp
+++ Source/Backend/Algorithm/Version2/Utils/Trie/Trie.cpp
@@ -1,20 +1,21 @@
 #include "Source/Backend/Algorithm/Version2/Utils/Trie/Trie.hpp"
-#include <algorithm>
 
-Algorithm::Version2::Utils::Trie::Trie()
+using Trie = Algorithm::Version2::Utils::Trie;
+
+Trie::Trie()
 {
 	root = std::make_unique<TrieNode>();
 }
 
-Algorithm::Version2::Utils::Trie::Trie(const std::vector<std::vector<bool>>& sequences) : Trie()
+Trie::Trie(const std::vector<std::vector<bool>>& sequences) : Trie()
 {
-	std::ranges::for_each(sequences, [&] (const std::vector<bool>& sequence)
+	for (const std::vector<bool>& sequence : sequences)
 	{
 		insert(sequence);
-	});
+	}
 }
 
-auto Algorithm::Version2::Utils::Trie::insert(const std::vector<bool>& sequence) -> void
+auto Trie::insert(const std::vector<bool>& sequence) -> void
 {
 	const auto node_exists = [] (const TrieNode* child) { return child != nullptr; };
 	TrieNode* current_node = root.get();
@@ -30,7 +31,7 @@ auto Algorithm::Version2::Utils::Trie::insert(const std::vector<bool>& sequence)
 	}
 }
 
-auto Algorithm::Version2::Utils::Trie::check_if_prefix_exists(const std::vector<bool>& sequence) const -> bool
+auto Trie::check_if_prefix_exists(const std::vector<bool>& sequence) const -> bool
 {
 	const auto node_exists = [] (const TrieNode* child) { return child != nullptr; };
 	TrieNode* current_node = root.get();
